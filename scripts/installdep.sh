6db22c106f29849091b7baeb39dae3fe7c969424
jcozar87 <javier.cozar@uclm.es>
2019-04-01 18:01:12 +0200

    Included useful scripts for windows and ubuntu

---
+++ scripts/installdep.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update
+apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev zlib1g-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
---
+++ scripts/setpath.bat
@@ -0,0 +1,1 @@
+sraer cmd /k SET "PATH=C:\Program Files\Python36\;C:\Program Files\Python36\Scripts\;%PATH%"
