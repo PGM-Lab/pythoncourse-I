377c18eadc33d16e26bd06b1b6672a6b8acba157
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2019-04-02 11:42:30 +0200

    Update setpath.bat
    Fixed typo in setpath.bat

--- scripts/setpath.bat
+++ scripts/setpath.bat
@@ -1,1 +1,1 @@
-sraer cmd /k SET "PATH=C:\Program Files\Python36\;C:\Program Files\Python36\Scripts\;%PATH%"
+start cmd /k SET "PATH=C:\Program Files\Python36\;C:\Program Files\Python36\Scripts\;%PATH%"
